2a89286ccef8ff7636da10ea893c05ef723ca6cf
Maks Melnyk <maks.melnik1611@gmail.com>
2024-04-24 16:01:38 +0300

    add test endpoint

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return 'Server is running...';
+});
+
+Route::get('/test', function (){
+    return 'Test Endpoint';
 });
