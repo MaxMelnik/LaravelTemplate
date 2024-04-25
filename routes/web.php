f63231a6a3dfc5a2420c908105499b3cbe211be7
Maks Melnyk <maks.melnik1611@gmail.com>
2024-04-25 17:42:51 +0300

    implement TestController

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
     // return view('welcome');
     return 'Server is running...';
 });
 
-Route::get('/test', function (){
-    return 'Test Endpoint';
-});
+Route::get('/test', [TestController::class, 'index']);
